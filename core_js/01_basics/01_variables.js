4d13baf6ac13b6c20c5ac7e27d6497f5ece03ea3
Ashutosh Gaurav <mr.gaurav9554@gmail.com>
2024-04-24 15:49:10 +0530

    variables in js

--- core_js/01_basics/01_variables.js
+++ core_js/01_basics/01_variables.js
@@ -1,1 +1,22 @@
-console.log('gaurav')
+const accountId = 1456;
+let accountName = "Ashutosh Gaurav";
+var accountPassword = "password";
+accountCity = "Lucknow"; // No Error
+let accountState;
+
+// accountId = 1234 // Error: Assignment to constant variable.
+accountName = "Ashutosh"; // No Error
+accountPassword = "newpassword"; // No Error
+
+/*
+prefer not to use var
+because of issues like hoisting and scoping 
+*/
+
+console.table([
+  accountId,
+  accountName,
+  accountPassword,
+  accountCity,
+  accountState,
+]);
