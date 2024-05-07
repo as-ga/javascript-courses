e4dd8a6cbda8bf4462c4da619b30c5126ac9b36b
Ashutosh Gaurav <mr.gaurav9554@gmail.com>
2024-05-07 23:59:26 +0530

    create or setup react app with vite

---
+++ react/01vitereact/src/chai.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Chai() {
+  return (
+    <React.Fragment>
+      <h3>chai is ready</h3>
+      <p>chai is a good drink</p>
+    </React.Fragment>
+  );
+}
+
+export default Chai;
