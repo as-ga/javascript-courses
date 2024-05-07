3158ca75a3a2c08e96678358bd5e6fd1c2dd6aaf
Ashutosh Gaurav <mr.gaurav9554@gmail.com>
2024-05-07 20:39:02 +0530

    create core react app

---
+++ react/01basicreact/src/Chai.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Chai() {
+  return (
+    <React.Fragment>
+      <h2>chai in react</h2>
+      <p>chai is a good drink</p>
+    </React.Fragment>
+  );
+}
+
+export default Chai;
