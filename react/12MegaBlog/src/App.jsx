58af057375a7180d7f5f46b848898c753c8307d0
Ashutosh Gaurav <mr.gaurav9554@gmail.com>
2024-05-09 17:59:55 +0530

    add redux

--- react/12MegaBlog/src/App.jsx
+++ react/12MegaBlog/src/App.jsx
@@ -1,8 +1,36 @@
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Header, Footer } from "./components";
+
 function App() {
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login(userData));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-500">
+      <div className="w-full block">
+        <Header />
+        <main>TODO: {/* <Outlet /> */}</main>
+        <Footer />
+      </div>
+    </div>
+  ) : (
+    <div>Loading...</div>
   );
 }
 
