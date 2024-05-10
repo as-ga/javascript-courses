c1d0748f749d6ad49f5c7a336a99ababe25c5c2d
Ashutosh Gaurav <mr.gaurav9554@gmail.com>
2024-05-10 19:22:55 +0530

    fix errors in redux

--- react/12MegaBlog/src/store/authSlice.js
+++ react/12MegaBlog/src/store/authSlice.js
@@ -1,20 +1,17 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {
-  status: "false",
-  userData: null,
-};
+const initialState = { status: false, userData: null };
 
 const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
-    login(state, action) {
-      state.status = "true";
-      state.userData = action.payload;
+    login: (state, action) => {
+      state.status = true;
+      state.userData = action.payload.userData;
     },
-    logout(state) {
-      state.status = "false";
+    logout: (state) => {
+      state.status = false;
       state.userData = null;
     },
   },
